76947d4240ef7a22e500d316d80f1d6e4416326a
Adonay Mendez <adonay.mendez255@gmail.com>
2025-08-13 17:55:19 -0400

    Variables

---
+++ script.js
@@ -0,0 +1,81 @@
+//! JS Basics
+
+//? Print content in the output/console
+
+// console.log(`Hello`);
+// console.log(`I like pizza`);
+
+//? Aleart pop out on screen
+
+// window.alert(`This is an alert`);
+// window.alert(`I like pizza`);
+
+//? Add Content to element
+
+// document.getElementById("myH1").textContent =  `Hello`;
+// document.getElementById("myP").textContent = `I like pizza!`
+
+
+//! Variables
+
+//* variables = A container that stores a value inside and 
+//*             behaves as if it were that value that was contained
+
+// 1. declaration  let x;
+// 2. assignment   x = 100; 
+// 3. console.log(x); 
+// 4. output = 100 Here the container is behaving as if it were the value inside because the output is 100
+
+//? Types of varibales / dataTypes
+
+//* To checck the type of varible console.log(typeof + variable name);   
+
+//* Numbers 
+// let age = 25; 
+// let price = 10.99;
+// let gpa = 2.1;
+// console.log(`You are ${age} years old`);  
+// console.log(`The price is $ ${price}`);
+// console.log(`Your gpa is: ${gpa}`);
+// Output: 
+// You are 25 years old
+// The price is $ 10.99
+// Your gpa is: 2.1
+
+
+//*Strings 
+// let firstName = "Adonay";
+// let favoriteFood = "Sushi";
+// let email = "adonay.mendez255@gmail.com"
+// console.log(typeof firstName);
+// console.log(`Your name is ${firstName}`);
+// console.log(`You like ${favoriteFood}`);
+// console.log(`Your email is ${email}`);
+// Output:
+// Your Name is Adonay 
+// You like Sushi
+// Your email is adonay.mendez255@gmail.com
+
+
+//*Booleans
+// let online = false;
+// let forSale = true;
+// let isStudent = true; 
+// console.log(`Adonay is online: ${online}`); 
+// console.log (`Is this car for sale: ${forSale}`);
+// console.log(`Enrolled: ${isStudent}`);
+// Output:
+// Adonay is online: false 
+// Is this car for sale: true
+// Enrolled: true
+
+//! Variables - Wrap up 
+
+// let fullName = "Adonay Mendez";
+// let age = 19;
+// let isStudent = true;
+
+// document.getElementById("p1").textContent = `Your name is ${fullName}`;
+// document.getElementById("p2").textContent = `You are ${age} years old`;
+// document.getElementById("p3").textContent = `Enrolled: ${isStudent}`;
+
