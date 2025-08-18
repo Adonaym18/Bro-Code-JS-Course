5e49d6565ed5fbfe08096ec10d65154e5de646cd
Adonay Mendez <adonay.mendez255@gmail.com>
2025-08-18 00:27:39 -0400

    Accept User Input

--- script.js
+++ script.js
@@ -1,44 +1,20 @@
-//! Arithmetic Operatiors
-//* arithmetic opertatiors = operands = values, variables, etc..)
-                            //* operatiors = (+ - * /)
-                              // ex. 11 = x + 5;
-                                // 11/5/x = operand
-                                // =/+ = operator
-
-// let students = 30; 
-
-// students = students + 1; 
-// students = students - 1;
-// students = students * 2;
-// students = students / 2;
-// students = students **2;
-// let extraStudents = students % 2; - modulus operator
-
-//* Shortcuts - augumented assignmnet operators 
-
-// students += 1;
-// students -= 1;
-// students *=2;
-// students /=2;
-// students **=2;
-// students %=2;
-
-//* increment and decrement operators  
-
-// students++; - increment 
-// students--; - decrement
+//! How to accept user input 
 
 /* 
-    Operator Presidence
-    1. parenthesis ()
-    2. exponets
-    3. multiplication, division, modulo 
-    4. addition & subtraction
+  1. Easy Way =  window prompt 
+    
+    let username = window.prompt("What's your username")
+    console.log(username); 
+*/
 
-example: 
-  
-  let result = 1 + 2 * 3 + 4 ** 2; 
-  console.log(result);
-  output = 23
+/*2. Professional Way = HTML textbox 
+
+let username;
+
+document.getElementById("mySubmit").onclick = function(){
+  username = document.getElementById("myText").value;
+  document.getElementById("H1").textContent = `Hello ${username}`;
+
+}
 
 */
