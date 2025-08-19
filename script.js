de67cc6f443d2a6df7c598179e66bd8e6ba220c1
Adonay Mendez <adonay.mendez255@gmail.com>
2025-08-19 09:14:27 -0400

    Type Conversion

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-//! How to accept user input 
+// type conversion = a way to change the datatype of a value into another 
+//                    (strings, numbers, booleans)
 
-/* 
-  1. Easy Way =  window prompt 
-    
-    let username = window.prompt("What's your username")
-    console.log(username); 
-*/
+// Eample 1
 
-/*2. Professional Way = HTML textbox 
+//   let age = window.prompt("How old are you?"); 
+//   age = Number(age); 
+//   age += 1; 
+//   console.log(age, typeof age); 
 
-let username;
+// Example 2 
 
-document.getElementById("mySubmit").onclick = function(){
-  username = document.getElementById("myText").value;
-  document.getElementById("H1").textContent = `Hello ${username}`;
+// let x = "0";
+// let y = "0";
+// let z = "";
 
-}
+// x = Number(x); 
+// y = String(y); 
+// z = Boolean(z);
 
-*/
+// console.log(x, typeof x);
+// console.log(y, typeof y);
+// console.log(z, typeof z);
