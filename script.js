a48b713104e78d59310d74350071cba810977e43
Adonay Mendez <adonay.mendez255@gmail.com>
2025-08-19 19:56:06 -0400

    Math Object

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-//  COUNTER PROGRAM
-
-const decreaseBtn = document.getElementById("decreaseBtn");
-const resetBtn = document.getElementById("resetBtn");
-const increaseBtn = document.getElementById("increaseBtn");
-const countLable = document.getElementById("countLabel");
-let count = 0; 
-
-increaseBtn.onclick = function() {
-  count++; 
-  countLable.textContent = count; 
-}
+// Math = built-in object that provides a
+          //collection of properties and methods
 
 
-resetBtn.onclick = function() {
-  count = 0;
-  countLable.textContent = count;  
-}
+// Math.PI - pi
+// Math.E - Euler's Number
 
-decreaseBtn.onclick = function () {
-  count--; 
-  countLable.textContent = count;
-}
+let x = 3; 
+let y = 2;
+let z = 1;
 
+//z = Math.round() - Round
+//z = Math.floor(x) - Rounds down
+//z = Math.ceil(x) -  Rounds Up
+//z = Math.trunc(x) - Eliminates Decimal Portion
+//z = Math.pow(x, y) - Raises the value on the left to the power of the value on the right
+//z = Math.sqrt(x) - Sqaure Root
+//z = Math.log(x); - Natural Log
+// z = Math.sin(x); - sin
+// z = Math.cos(x); - cos
+// z = Math.tan(x); - tan
+// z = Math.abs(x); - absolute value
+// z = Math.sign(x) - to the sign of a number 
 
-
+// let max = Math.max(x, y, z); - to find the maximum value from amoung all 3 varibales
+// let min = Math.min(x, y, z);  - to find the minimum value from amoung all 3 varibales
