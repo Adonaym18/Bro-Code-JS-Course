dd7b6a8b81329ee8e3a71daf22954b93cb01e606
Adonay Mendez <adonay.mendez255@gmail.com>
2025-08-19 09:53:44 -0400

    Constants

--- script.js
+++ script.js
@@ -1,23 +1,15 @@
-// type conversion = a way to change the datatype of a value into another 
-//                    (strings, numbers, booleans)
+// const (constants) = are variables that cannot be changed 
 
-// Eample 1
+const PI = 3.14159
+let radius;
+let circumference; 
 
-//   let age = window.prompt("How old are you?"); 
-//   age = Number(age); 
-//   age += 1; 
-//   console.log(age, typeof age); 
 
-// Example 2 
+ document.getElementById("submit").onclick = function () {
+  radius = document.getElementById("input").value; 
+  radius = Number(radius); 
+  circumference = 2 * PI * radius;
+  document.getElementById("result").textContent = circumference + "cm"; 
+  
+}
 
-// let x = "0";
-// let y = "0";
-// let z = "";
-
-// x = Number(x); 
-// y = String(y); 
-// z = Boolean(z);
-
-// console.log(x, typeof x);
-// console.log(y, typeof y);
-// console.log(z, typeof z);
