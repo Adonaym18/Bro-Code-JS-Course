a4f93f68df6099b87ec5af99612b97918a2782b3
Adonay Mendez <adonay.mendez255@gmail.com>
2025-08-22 20:16:18 -0400

    String Methods

--- script.js
+++ script.js
@@ -1,65 +1,26 @@
-// SWITCH = Can be an efficient replacement to many else if statements 
+// String methods = allow you to manipulate and work with text (strings)
 
-//EXAMPLE 1
-
-// let day = 7; 
-
-//   switch(day){
-//     case 1:
-//       console.log(`It is Monday`); 
-//       break; // to break out of the case and execute the code after it
-
-//     case 2:
-//       console.log(`It is Tuesday`); 
-//       break; 
-
-//     case 3:
-//       console.log(`It is Wensday`); 
-//       break; 
-
-//     case 4:
-//       console.log(`It is Thursday`); 
-//       break; 
-
-//     case 5:
-//       console.log(`It is Friday`); 
-//       break; 
-    
-//     case 6:
-//       console.log(`It is Saturday`); 
-//       break; 
-    
-//     case 7:
-//       console.log(`It is Sunday`); 
-//       break; 
-
-//      default: 
-//       console.log(`${day} is not a day`); 
-
-//   }
+let userName = "adonayM"; 
+// .charAt() = allows to to select charaters from a string starting form the first one which would be 0.
+// .indexOf(a) = will return the index first occurance of a charater which is 0. 
+// .lastIndexOf(a) = will return the index of the last occurance  which is 4. 
+// .length  = To get the length of the string. 
+// .trim();   = to remove extra white spacing
+// .toUpperCase() = Change all charaters to upper case
+// .toLowerCase() = Change all charaters to lower case
+// .repeat(#) = repeats a string the amount in the parenthesis
+// .startsWith() = to determine if a string starts with whatever you place inside of the parenthesis
+//                              could be useful ehrn using if statements
+// .endsWith() = oppposite of startsWith()
+// .include() = does the string in this case include whatever condition place inside the ()?
+console.log(username);  
 
 
-// EXAMPLE 2
+let phoneNumber = "123-456-7890";
+// phoneNumber = phoneNumber.replaceAll("-" , " "); = This replaces the - with and empty space. 
+// phoneNumber = phoneNumber.padStart(15, "0"); = Fill the start of the string with "0" until the string reaches a length of 15 charaters long
+// phoneNumber = phoneNumber.padEnd(15, "0"); = Fill the end of the string with "0" until it reaches a length of 15 charaters long
+ console.log(phoneNumber);
 
-let testScore = 65; 
-let letterGrade; 
-
-switch(true) {
-  case testScore >= 90:
-      letterGrade = "A"; 
-      break; 
-  case testScore >= 80:
-      letterGrade = "B";
-      break; 
-  case testScore >= 70:
-      letterGrade = "C"
-      break; 
-  case testScore >= 60:
-      letterGrade = "D"
-      break; 
-      default: letterGrade = "F"; 
-}
-  
-console.log(letterGrade); 
 
 
