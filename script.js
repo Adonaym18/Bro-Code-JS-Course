450ed6eda613f3eda5c06893a7ab5917755899e1
Adonay Mendez <adonay.mendez255@gmail.com>
2025-08-24 00:09:11 -0400

    String Slicing

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
-// String methods = allow you to manipulate and work with text (strings)
+// string slicing = creating a subsrting 
+//                   from a portion of another string
 
-let userName = "adonayM"; 
-// .charAt() = allows to to select charaters from a string starting form the first one which would be 0.
-// .indexOf(a) = will return the index first occurance of a charater which is 0. 
-// .lastIndexOf(a) = will return the index of the last occurance  which is 4. 
-// .length  = To get the length of the string. 
-// .trim();   = to remove extra white spacing
-// .toUpperCase() = Change all charaters to upper case
-// .toLowerCase() = Change all charaters to lower case
-// .repeat(#) = repeats a string the amount in the parenthesis
-// .startsWith() = to determine if a string starts with whatever you place inside of the parenthesis
-//                              could be useful ehrn using if statements
-// .endsWith() = oppposite of startsWith()
-// .include() = does the string in this case include whatever condition place inside the ()?
-console.log(username);  
+//                    string.slice(start, end)
 
+//      Exmple 1 
 
-let phoneNumber = "123-456-7890";
-// phoneNumber = phoneNumber.replaceAll("-" , " "); = This replaces the - with and empty space. 
-// phoneNumber = phoneNumber.padStart(15, "0"); = Fill the start of the string with "0" until the string reaches a length of 15 charaters long
-// phoneNumber = phoneNumber.padEnd(15, "0"); = Fill the end of the string with "0" until it reaches a length of 15 charaters long
- console.log(phoneNumber);
+// const fullName = "Adonay Mendez"
+
+// let firstName = fullName.slice(0, 6);
+// let lastName = fullName.slice(7);
+
+// let firstChar = fullName.slice(0 , 1); 
+// let lastChar = fullName.slice(-1);
+
+// let firstName = fullName.slice(0, fullName.indexOf(" ")); 
+// let lastName = fullName.slice(fullName.indexOf(" ") + 1);
+
+// console.log(firstName);
+// console.log(lastName); 
 
 
+// Example 2 
 
+const email = "adonay.mendez255@gmail.com"; 
+
+let username = email.slice(0, email.indexOf("@")); 
+let extenstion = email.slice(email.indexOf("@") + 1); 
+
+console.log(username); 
+console.log(extenstion); 
