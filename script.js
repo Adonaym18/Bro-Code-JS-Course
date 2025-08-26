371e7ec11717b076df63aeee68c9fbc9f209870f
Adonay Mendez <adonay.mendez255@gmail.com>
2025-08-26 09:49:30 -0400

    While Loop

--- script.js
+++ script.js
@@ -1,28 +1,60 @@
-//* Strict Equality
-//*  = assignment operator 
-//*  == comparison operator (comare if values are equal)
-//*  === strict equlity operator (compare if values & datatype are equal)
-//*  != inequality operator 
-//* !== strict inequality operator
+// while loop = repeat some code WHILE some condition is true 
 
 
-// Example 1 - == vs ===
-// const PI = 3.14 
+//* Example 1 - while loop 
 
-// if (PI === "3.14") {
-//   console.log("That is PI"); 
+// let username = "";
+
+//   while(username === "" || username === null) {
+//   username = window.prompt("Enter your name"); 
 // }
-//   else{
-//     console.log("That is not PI")
+// console.log(`Hello ${username}`);
+
+
+//* Example 2 - do while loop 
+
+// let username; 
+
+// do {
+//     username = window.prompt("Enter your name"); 
+// }while(username === "" || username === null)
+// console.log(`Hello ${username}`);
+
+//* Example 3 
+
+// let loggedIn = false; 
+// let username; 
+// let password; 
+
+// while(!loggedIn){
+//   username = window.prompt("Enter your username: ");
+//   password = window.prompt("Enter your password: ");
+
+//   if(username === "myUsername"  && password == "myPassowrd"){
+//     loggedIn = true; 
+//     console.log("You are logged in!")
 //   }
+//   else{
+//     console.log("Invalid credentials! Please try again"); 
+//   }
+// }
 
 
-// Example 2 - != vs !===
- const PI = 3.14 
+//* Example 3 - using do while loop
 
-if (PI != "3.14") {
-  console.log("That is NOT PI"); 
-}
-  else{
-    console.log("That is PI")
+let loggedIn = false; 
+let username; 
+let password; 
+
+do{
+  username = window.prompt("Enter your username: ");
+  password = window.prompt("Enter your password: ");
+
+  if(username === "myUsername"  && password == "myPassowrd"){
+    loggedIn = true; 
+    console.log("You are logged in!")
   }
+  else{
+    console.log("Invalid credentials! Please try again"); 
+  }
+}while(!loggedIn);
