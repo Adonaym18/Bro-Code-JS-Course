4ac93672a00ca99d6db0ef9b1e9bb933b7f16050
Adonay Mendez <adonay.mendez255@gmail.com>
2025-08-28 11:38:56 -0400

    function

--- script.js
+++ script.js
@@ -1,42 +1,77 @@
-// NUMBER GUESSING GAME
+// function = A section of reusable code. 
+//            Declare code once, use it whenever you want. 
+//            Call the function to execute that code
 
-const minNum = 1;
-const maxNum = 100; 
-const answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+//*  EX. 1
+// function happyBirthday(username, age) {
+//     console.log("Happy birthday to you!");
+//     console.log("Happy birthday to you!");
+//     console.log(`Happy birthday dear ${username}`);
+//     console.log("Happy birthday to you!");
+//     console.log(`You are ${age} years old`);
+// }
 
-let attempts = 0; 
-let guess; 
-let running = true; 
+// happyBirthday("Adonay", 19);
+// happyBirthday("spongebob", 30);
 
-while(running){
+//* Ex. 2 - the return keyword
 
-  guess = window.prompt(`Guess a number between ${minNum} - ${maxNum}`); 
-  guess = Number(guess);
-  
-  if(isNaN(guess)) {
-    window.alert(`Please enter a valid number`);
+function add(x, y){
+  let result = x + y; 
+  return result; 
+
+  // or you could just use return x + y 
+  //  the console.log(add(2,3) outside the function
+}
+
+function subtract(x, y){
+  return x - y;  
+}
+
+function multiply(x , y){
+  return x * y; 
+}
+
+function divide( x , y){
+  return x / y; 
+}
+
+function isEven(number){
+   
+  if(number % 2 === 0){
+    return true; 
   }
-
-  else if(guess < minNum || guess > maxNum){
-    window.alert("Please enter a valid number"); 
-  }
-
 
   else{
-    attempts++;
-     if(guess < answer){
-      window.alert("TOO LOW! TRY AGAIN"); 
-     }
-
-     else if(guess > answer){
-      window.alert("TOO HIGH! TRY AGAIN"); 
-     }
-
-     else {
-       window.alert(`CORRECT! The answer was ${answer}. It took you ${attempts} attempts.`);
-      running = false;
-
-     }
+    return false; 
   }
-
+  
+  //* using tenary operator 
+  // return number & 2 === 0 ? true : false; 
 }
+// console.log(isEven(10));
+
+
+//* Ex. 4
+
+function isValidEmail(email){
+  // if(email.includes("@")){
+  //   return true; 
+  // }
+
+  // else {
+  //   return false
+  // }
+
+  return email.includes("@") ? true : false; 
+}
+
+console.log(isValidEmail("adonay.mendez255@gmail.com"));
+
+
+
+
+
+
+// let answer = add(2, 3); 
+// console.log(answer)
