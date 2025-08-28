8875818782c8930a52a30ed957ac8842bd97748c
Adonay Mendez <adonay.mendez255@gmail.com>
2025-08-28 12:01:56 -0400

    Variable Scope

--- script.js
+++ script.js
@@ -1,77 +1,23 @@
-// function = A section of reusable code. 
-//            Declare code once, use it whenever you want. 
-//            Call the function to execute that code
+// variable scope = where a variable is recongnized 
+//                  and accessible (local vs global); 
 
-//*  EX. 1
-// function happyBirthday(username, age) {
-//     console.log("Happy birthday to you!");
-//     console.log("Happy birthday to you!");
-//     console.log(`Happy birthday dear ${username}`);
-//     console.log("Happy birthday to you!");
-//     console.log(`You are ${age} years old`);
-// }
 
-// happyBirthday("Adonay", 19);
-// happyBirthday("spongebob", 30);
+// Global Scope - could be accessed from anywhere 
+let x = 3;
+function2();
 
-//* Ex. 2 - the return keyword
 
-function add(x, y){
-  let result = x + y; 
-  return result; 
+// Local Scope - stricly private to its function 
+//               which is why you can create to with similiar variable 
+//               names and not cause an error. 
 
-  // or you could just use return x + y 
-  //  the console.log(add(2,3) outside the function
+function function1(){
+  // let x = 1; 
+  console.log(x);
+}
+function function2(){
+  // let x = 2; 
+  console.log(x);
 }
 
-function subtract(x, y){
-  return x - y;  
-}
 
-function multiply(x , y){
-  return x * y; 
-}
-
-function divide( x , y){
-  return x / y; 
-}
-
-function isEven(number){
-   
-  if(number % 2 === 0){
-    return true; 
-  }
-
-  else{
-    return false; 
-  }
-  
-  //* using tenary operator 
-  // return number & 2 === 0 ? true : false; 
-}
-// console.log(isEven(10));
-
-
-//* Ex. 4
-
-function isValidEmail(email){
-  // if(email.includes("@")){
-  //   return true; 
-  // }
-
-  // else {
-  //   return false
-  // }
-
-  return email.includes("@") ? true : false; 
-}
-
-console.log(isValidEmail("adonay.mendez255@gmail.com"));
-
-
-
-
-
-
-// let answer = add(2, 3); 
-// console.log(answer)
