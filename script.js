b8f8053c0bc14b55b6b7fa1f81f4a9051414bdae
Adonay Mendez <adonay.mendez255@gmail.com>
2025-09-01 23:31:34 -0400

    Spread Operator

--- script.js
+++ script.js
@@ -1,43 +1,28 @@
-// array = a variable like structure that can hold more than 1 value
+//* Spread Operator = ... allows an iterable such as an 
+//                      array or string to be expanded 
+//                      into sperate elements 
+//                      (unpacks the elements)
 
-let fruits = ["apple" , "orange", "banana", "mango"]; 
+//* Example with numbers 
 
-// fruits[3] = "mango"; 
-// fruits.push("cocnut"); - To push/add an element to the end of the array 
-// fruits.pop(); - removes the last element
-// fruits.unshift("mango"); - Adds element to the beginning of an array
-// fruits.shift(); - Removes an element from the beginning of an array
+// let numbers = [1, 2, 3, 4, 5];
+// let max = Math.max(...numbers);
+// let min = Math.min(...numbers); 
 
-// console.log(fruits[0]);
-// console.log(fruits[1]);
-// console.log(fruits[2]);
-// console.log(fruits[3]);
+// console.log(min);
 
-// let numOffruits = fruits.length; - To get the length of an array; 
-// let index = fruits.indexOf("apple"); - To find a particular fruit in array 
-// console.log(index); 
+//* Example with strings
 
-// To loop through elements and display them
+// let username =  "Adonay"
+// let letters = [...username].join("-")
+// console.log(letters);
 
-// for(let i = 1; i < fruits.length; i+=2) {
-//   console.log(fruits[i]);
-// }
+//* Example 1 
+let fruits = ["apple", "orange", "banana"];
+let vegatables = ["carrots", "celery", "potatoes"]; 
+let foods = [...fruits, ...vegatables, "eggs", "milk"];
 
-
-// to do it in reverse
-// for(let i = fruits.length - 1; i >= 0; i--){
-//   console.log(fruits[i]);
-// }
-
-
-// fruits.sort(); - to sort in alphabetical order
-// fruits.sort().reverse(); - to sort in reverse alphabetical order
-
-
-// enchanced for loop (short cut)
-for(let fruit of fruits){
-  console.log(fruit);
-}
+console.log(foods); 
 
 
 
