3b850d749b5afef6751c8b639822b35f78c64880
Adonay Mendez <adonay.mendez255@gmail.com>
2025-09-04 22:05:55 -0400

    Dice Roller Program

--- script.js
+++ script.js
@@ -1,65 +1,19 @@
-// rest parameters = (...rest) allow a funciton to work with a variable
-//                    number of arguments by bundling then in an array
+// DICE ROLLER PROGRAM
 
-//                    spread =  expands an array into seprarte elements
-//                    rest = bundles seperate elements into an array
+function rollDice(){
+  
+    const numOfDice= document.getElementById("numOfDice").value; 
+    const diceResult = document.getElementById("diceResult");
+    const diceImages = document.getElementById("diceImages");
+    const values = []; 
+    const images = []; 
 
-//* Example 1
+    for(let i = 0; i < numOfDice; i++){
+      const value = Math.floor(Math.random() * 6) + 1; 
+      values.push(value); 
+      images.push(`<img src="dice_images/${value}.png" alt="Dice ${value}">`); 
+    }
 
-// function openFridge(...foods) {
-//   console.log(...foods); // to expand the array back into separate elements
-// }
-
-// Stick all e
-// function getFood(...foods){
-//   return foods; 
-// }
-
-// const food1 = "pizza";
-// const food2 = "hamburger";
-// const food3 = "suhsi";
-// const food4 = "hotdog";
-// const food5 = "ramen";
-
-
-// openFridge(food1, food2, food3, food4, food5); // This is the step that places the variables into the array
-
-// const foods = getFood(food1, food2, food3, food4, food5);
-// console.log(foods);
-
-
-//* Example 2 
-// function sum(...numbers){
-//   let result = 0;
-
-//   for(let number of numbers) {
-//     result += number; 
-//   }
-//   return result; 
-// } 
-
-// const total = sum(1, 2, 3, 4, 5); 
-
-// console.log(`Your total is $${total}`); 
-
-// function getAverage(...numbers){
-//   let result = 0;
-
-//   for(let number of numbers) {
-//     result += number; 
-//   }
-//   return result / numbers.length; 
-// } 
-
-// const total = getAverage(75 , 100, 85, 90, 50);
-// console.log(total); 
-
-//* Example 3 = using the rest parameters to comine strings into one
-
-function combineStrings(...strings){
-  return strings.join(" ");
+    diceResult.textContent = `dice: ${values.join(", ")}`; 
+    diceImages.innerHTML = images.join(''); 
 }
-
-const fullName = combineStrings("Mr.", "Spongebob", "Squarepants", "III");
-
-console.log(fullName);
