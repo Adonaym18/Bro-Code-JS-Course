4b816660bda3fcf6432115b839386cf625c766c2
Adonay Mendez <adonay.mendez255@gmail.com>
2025-10-09 11:09:38 -0400

    Function Expressions

--- script.js
+++ script.js
@@ -1,31 +1,86 @@
-// .reduce() = reduce the elements of an array 
-//              to a single value 
+// function declaration = define a resuable block of code 
+//                        that performes a specific task
 
-//* Ex1 
 
-// const prices = [5, 30, 10, 25, 15, 20];
-
-// const total = prices.reduce(sum); 
-
-// console.log(`$${total.toFixed(2)}`)
-
-// function sum (accumulator, element){
-//   return accumulator + element; 
+// function hello(){
+//   console.log("Hello"); 
 // }
 
-//* Ex2 
 
-const grades = [75, 50, 90, 80, 65, 95]; 
+// function expressions = a way to define function as 
+//                        values or varibales
 
-const maximum = grades.reduce(getMax); 
-const minimum = grades.reduce(getMin); 
+//  1. Callbacks in asynchronus operations 
+//  2. Higher-Order functions 
+//  3. Closures 
+//  4. Event Listeners
 
-console.log(minimum)
 
-function getMax(accumulator, element){
-  return Math.max(accumulator, element);
-}
 
-function getMin(accumulator, element){
-  return Math.min(accumulator, element);
-}
+//* Ex1 - As a variable
+
+// const hello = function () {
+//   console.log("Hello");
+// }
+
+// hello();
+
+//* Ex2 - As a value 
+
+// const hello = function () {
+//   console.log("Hello");
+// }
+
+// setTimeout(hello, 3000);
+
+//* Ex3 - Passing a function as an argument
+
+// setTimeout(function(){
+//   console.log("Hello"); 
+// }, 3000); 
+
+//* Ex4 - Passing a function as an argument
+
+//* Using a function declaration 
+
+    // const numbers = [1, 2, 3, 4, 5, 6]; 
+
+    // const sqaured = numbers.map(sqaure)
+
+    // function sqaure(element){
+    //    return Math.pow(element, 2); 
+    // }
+
+
+    // console.log(sqaured); 
+
+//* Using a function expression
+
+    // const numbers = [1, 2, 3, 4, 5, 6]; 
+
+    // const sqaured = numbers.map(function(element){
+    //        return Math.pow(element, 2); 
+    //     }); 
+
+    // console.log(sqaured); 
+
+//* Ex - Cubed / Practice using .map(), .filter(), .reduce()
+
+    const numbers = [1, 2, 3, 4, 5, 6]; 
+    const cubed = numbers.map(function(element){
+      return Math.pow(element, 3); 
+    }); 
+
+    const evenNums = numbers.filter(function(element) {
+      return element % 2 === 0;
+    }); 
+
+    const oddNums = numbers.filter(function(element) {
+      return element % 2 !== 0;
+    }); 
+
+    const total = numbers.reduce(function(acumulator, element){
+      return acumulator + element;
+    })
+
+    console.log(total); 
