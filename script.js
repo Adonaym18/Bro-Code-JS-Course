aec23ff3c554f829b57334594e4e6e7ab1a4f647
Adonay Mendez <adonay.mendez255@gmail.com>
2025-10-18 16:47:07 -0400

    super

--- script.js
+++ script.js
@@ -1,51 +1,62 @@
-// inheritance = allows a new class to inherit properties and methods 
-//               from an existing class (parent -> child)  
-//               helps with code reusability
+// super = keyword is used in classes to call the constructor or 
+//         access the properties and methods of a parent (superClass)
+//         this = this object 
+//         super = the parent
 
 class Animal {
-    alive = true; 
 
-    eat(){
-        console.log(`This ${this.name} is eating`);
-    }
+  constructor(name, age){
+    this.name = name; 
+    this.age = age; 
+  }
 
-    sleep(){
-        console.log(`This ${this.name} is sleeping`);
-    }
+  move(speed){
+    console.log(`The ${this.name} moves at a speed of ${speed}mph`)
+  }
 }
 
 class Rabbit extends Animal {
-    name = "rabbit"; 
+  constructor(name, age, runSpeed){
+    super(name, age);
+    this.runSpeed = runSpeed; 
+  }
 
-    run(){
-        console.log(`This ${this.name} is running`);
-    }
-
+  run(){
+    console.log(`This ${this.name} can run`);
+    super.move(this.runSpeed);
+  }
 }
 
 class Fish extends Animal {
-    name = "fish";
+  constructor(name, age, swimSpeed){
+    super(name, age);
+    this.swimSpeed = swimSpeed; 
+  }
 
-     swim(){
-        console.log(`This ${this.name} is swimming`);
-    }
+   swim(){
+    console.log(`This ${this.name} can swim`);
+    super.move(this.swimSpeed);
 
+  }
 }
 
 class Hawk extends Animal {
-    name = "hawk";
+  constructor(name, age, flySpeed){
+    super(name, age);
+    this.flySpeed = flySpeed; 
+  }
 
-     fly(){
-        console.log(`This ${this.name} is flying`);
-    }
+   fly(){
+    console.log(`This ${this.name} can fly`);
+    super.move(this.flySpeed);
 
+  }
 }
 
-const rabbit = new Rabbit();
-const fish = new Fish();
-const hawk = new Hawk();
+const rabbit = new Rabbit("rabbit", 1, 25);
+const fish = new Fish("fish", 2, 12);
+const hawk = new Hawk("hawk", 3, 50);
 
-console.log(rabbit.alive); 
-rabbit.eat(); 
-rabbit.sleep(); 
 rabbit.run();
+fish.swim();
+hawk.fly();
